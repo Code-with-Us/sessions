1ae36f26ff5fa4e98d0a74fb4a96bc6da57bd065
Rodrick Bloomfield <23622438+bloom305@users.noreply.github.com>
2018-08-28 22:40:25 -0400

    Post session code from React1

--- React1/demo/index.js
+++ React1/demo/index.js
@@ -6,3 +6,24 @@ var classes = {
   list: 'uk-list',
   todo: 'uk-card uk-card-secondary uk-width-1-3 uk-position-top-center uk-margin-large-top uk-box-shadow-xlarge'
 }
+
+var list = ['Learn React', 'Learn JSX', 'Put it all together', 'something else'];
+
+var e = React.createElement;
+
+var title = e('h1', { className: classes.title }, 'Todo List');
+var input = e('input', {className: classes.input, placeholder: 'New Item'}, null)
+var button = e('button', { className: classes.button }, 'Add');
+var form = e('form', null, [input, button]);
+
+function List(list) {
+  return e('ul', { className: classes.list }, list.map(function (item, index) {
+    return e('li', {key: index}, item)
+  }))
+}
+
+var cardBody = e('div', {className: classes.card}, [title, form, List(list)])
+var todoList = e('div', {className: classes.todo}, cardBody)
+
+var ROOT = document.querySelector('#root');
+ReactDOM.render(todoList, ROOT);
--- React1/demo/index2.js
+++ React1/demo/index2.js
@@ -6,3 +6,42 @@ const classes = {
   list: 'uk-list',
   todo: 'uk-card uk-card-secondary uk-width-1-3 uk-position-top-center uk-margin-large-top uk-box-shadow-xlarge'
 }
+
+function List(props) {
+  const { list } = props;
+  return (
+    <ul className={classes.list}>
+      {list.map((item, index) => <li key={index}>{item}</li>)}
+    </ul>
+  );
+}
+
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      list: ['Learn React', 'Learn JSX', 'Put it all together']
+    }
+  }
+
+
+  render() {
+    return (
+      <div className={classes.todo} >
+        <div className={classes.card} >
+          <h1 className={classes.title} >Todo List</h1>
+          <form>
+            <input type="text" className={classes.input} placeholder='New Item' />
+            <button className={classes.button}>Add</button>
+          </form>
+          <List list={this.state.list} />
+        </div>
+      </div>
+    )
+  }
+}
+
+const ROOT = document.querySelector('#root');
+ReactDOM.render(<TodoList />, ROOT);
